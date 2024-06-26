d81ec4d73e57ad560d55cffad3f4094d6c631686
Szymorek Oskar <oskar_szymorek@upl.local>
2024-06-27 00:07:24 +0200

    Create components for first project

---
+++ 03-pizza-menu/app/src/App.js
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Menu } from "./components/Menu";
+import { Footer } from "./components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
---
+++ 03-pizza-menu/app/src/components/Pizza.js
@@ -0,0 +1,9 @@
+export function Pizza() {
+  return (
+    <div>
+      <img src="img/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinaci, and ricotta cheese</p>
+    </div>
+  );
+}
