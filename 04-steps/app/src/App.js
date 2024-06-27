e181d8618d64e7e743967f6a2ec5566c892cf355
Szymorek Oskar <oskar_szymorek@upl.local>
2024-06-27 20:03:54 +0200

    New project with Hooks

---
+++ 04-steps/app/src/App.js
@@ -0,0 +1,45 @@
+const messages = [
+  "Learn React ✌️",
+  "Apply for jobs 👜",
+  "Invest your new income 😁",
+];
+
+export default function App() {
+  const step = 1;
+
+  const handlePrevious = () => {
+    alert("Previous");
+  };
+
+  const handleNext = () => {
+    alert("Next");
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={(handlePrevious) => alert("Previous")}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={(handleNext) => alert("Previous")}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
