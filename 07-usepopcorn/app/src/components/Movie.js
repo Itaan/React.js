ca4a71dfa020248a00cb29a32a34bcbc777d2cf8
Szymorek Oskar <oskar_szymorek@upl.local>
2024-07-06 01:36:39 +0200

    Added UseEffect

--- 07-usepopcorn/app/src/components/Movie.js
+++ 07-usepopcorn/app/src/components/Movie.js
@@ -5,7 +5,7 @@ export default function Movie({ movie }) {
       <h3>{movie.Title}</h3>
       <div>
         <p>
-          <span>🗓</span>
+          <span>📃</span>
           <span>{movie.Year}</span>
         </p>
       </div>
