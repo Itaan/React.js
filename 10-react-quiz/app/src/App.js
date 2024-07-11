a0045e0cf6ebbbdc723d40107022c8e88a9b5d8f
Oskar Szymorek <oskarszymorek1999@gmail.com>
2024-07-11 08:27:41 +0200

    Application has been finished

--- 10-react-quiz/app/src/App.js
+++ 10-react-quiz/app/src/App.js
@@ -9,13 +9,18 @@ import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
 import Finished from "./components/Finished";
+import Footer from "./components/Footer";
+import Timer from "./components/Timer";
 
+const SECS_PER_QUESTION = 30;
 const initialState = {
   questions: [],
   status: "loading",
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -35,6 +40,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -52,6 +58,21 @@ function reducer(state, action) {
         index: state.index++,
         answer: null,
       };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "reset":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -60,8 +81,8 @@ function reducer(state, action) {
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const numQuestions = state.questions.length;
-  const maxPossiblePoints = state.questions.reduce(
+  const numQuestions = state.questions?.length;
+  const maxPossiblePoints = state.questions?.reduce(
     (acc, curr) => acc + curr.points,
     0,
   );
@@ -101,13 +122,26 @@ export default function App() {
               dispatch={dispatch}
               answer={state.answer}
             />
-            <NextButton dispatch={dispatch} answer={state.answer} />
+            <Footer>
+              <Timer
+                dispatch={dispatch}
+                secondsRemaining={state.secondsRemaining}
+              />
+              <NextButton
+                dispatch={dispatch}
+                answer={state.answer}
+                numQuestions={numQuestions}
+                index={state.index}
+              />
+            </Footer>
           </>
         )}
         {state.status === "finished" && (
           <Finished
             points={state.points}
             maxPossiblePoints={maxPossiblePoints}
+            highscore={state.highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
